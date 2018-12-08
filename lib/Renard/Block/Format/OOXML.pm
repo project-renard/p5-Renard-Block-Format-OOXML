355336e2357d1bbee19a2c37832a5f91b2ad17a9
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-12-07 23:06:42 -0600

    Initial commit

---
+++ lib/Renard/Block/Format/OOXML.pm
@@ -0,0 +1,11 @@
+package Renard::Block::Format::OOXML;
+
+use strict;
+use warnings;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Block-Format-OOXML/>
+
+=cut
